77b8904f956499aaf354e20bbb5d8d66ede20e67
actionpotato <gww@walford.ca>
2017-07-25 12:33:56 -0700

    ex33b new test program

--- ex33a.py
+++ ex33a.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
+import time
+
 # Study drill for exercise 33
 
 def function_junction(funky):
     """This function does some counting"""
     interval = 0
+    the_nom = 101
     numbers = []
 
-    while interval < 101:
+    while interval < the_nom:
         print "The number is %d" % interval
         numbers.append(interval)
+
+        time.sleep(0.1)
 
         interval = interval + 1
         print "Moving on"
@@ -18,3 +23,5 @@ def function_junction(funky):
 print "Ok, here we go:"
 funkadelic = 0
 function_junction(funkadelic)
+
+print "The end."
