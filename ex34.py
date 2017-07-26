ada601504fbeb7e79a0c79746de924d2a4f5d02a
actionpotato <gww@walford.ca>
2017-07-26 16:05:49 -0700

    Fix typos in print

--- ex34.py
+++ ex34.py
@@ -13,4 +13,11 @@ for number in animals:
     time.sleep(0.5)
 
 
-print "The animal at 1, cardinal is the 2nd animal, ordinal: %s" % animals[1]
+print "The animal at 1, is a %s" % animals[1]
+time.sleep(1)
+
+print "The third (3rd) animal is at 2 and is %s" % animals[2]
+time.sleep(1)
+
+print "The first (1st) animal is at 0, and it is a %s" % animals[0]
+time.sleep(1)
