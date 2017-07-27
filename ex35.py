e69b8f4ce189e2c2782788ba7ad9c2060b776bbb
actionpotato <gww@walford.ca>
2017-07-27 10:21:35 -0700

    "Fixing bugs"

--- ex35.py
+++ ex35.py
@@ -11,27 +11,30 @@ def gold_room():
     else:
         dead("Man, learn to type a number.")
 
-        if how_much < 50
+        if how_much < 50:
             print "Nice, you're not greedy, you win!"
             exit(0)
         else:
             dead("You greedy bastard!")
 
 
-    def bear_room():
-        print "There is a bear here."
-        print "The bear has a bunch of honey."
-        print "The fat bear is in front of another door."
-        print "How are you going to move the bear?"
-        bear_moved = False
+def bear_room():
+    print "There is a bear here."
+    print "The bear has a bunch of honey."
+    print "The fat bear is in front of another door."
+    print "How are you going to move the bear?"
+    bear_moved = False
 
-        while True:
-            choice = raw_input("> ")
+    while True:
+        choice = raw_input("> ")
 
         if choice == "take honey":
             dead("The bear looks at you then slaps your face off.")
         elif choice == "taunt bear" and not bear_moved:
-            dead("The bear gets pissed off and chews your lef off.")
+            print "The bear has moved from the door. You can go through it now."
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leg off.")
         elif choice == "open door" and bear_moved:
             gold_room()
         else:
@@ -48,7 +51,7 @@ def cthulhu_room():
         start()
     elif "head" in choice:
         dead("Well that was tasty!")
-    else
+    else:
         cthulhu_room()
 
 def dead(why):
