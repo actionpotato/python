5b7b751b1d4dd5924e1d21a2d985423cac4661e0
actionpotato <gww@walford.ca>
2017-07-27 14:49:52 -0700

    "adding damage"

--- ex36.py
+++ ex36.py
@@ -15,21 +15,35 @@ dragon_damage = 0
 if d20 == 1:
     print "CRITICAL FAILURE! Talk about whiffing it!"
     #dead()
+    print dragon_damage
+    exit(0)
 if d20 < 5:
     print "Wow, talk about a miss!"
     #dragon()
+    print dragon_damage
+    exit(0)
 if d20 < 10:
     print "you missed, but barely stay upright."
     #dragon()
+    dragon_damage
+    exit(0)
 if d20 <= 15:
     print "Hit! you do damage!"
     #dragon() ++dragon_damage
+    dragon_damage += 1
+    print dragon_damage
+    exit(0)
 if d20 <= 19:
     print "You hit HARD!"
-    #dragon() ++dragon_damage X2
+    #dragon() ++dragon_damage X3
+    dragon_damage += 3
+    exit(0)
 if d20 == 20:
     print "Natural 20? instant kill!!!"
     #killed_dragon()
+    dragon_damage = 10
+    print dragon_damage
+    exit(0)
 else:
     print "That isn't supposed to happen... You find a glorkum.s"
     exit(0)
