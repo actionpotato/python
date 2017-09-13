b74aa7d950736492881218679cea2eaeda7d1719
actionpotato <gww@walford.ca>
2017-09-13 15:45:28 -0700

    Testing some stuff from Art of Programming

---
+++ cprog1_1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+m = int(raw_input('Enter an integer for m: '))
+n = int(raw_input('Enter an integer for n: '))
+# Calculate
+r = m / n
+print (str(m) + '/' + str(n) + '=' + str(r))
